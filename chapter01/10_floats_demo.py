426c37b6f1c14e782319262d294d105631e6e48f
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-06-23 19:25:38 +0300

    print PI with f string

---
+++ chapter01/10_floats_demo.py
@@ -0,0 +1,9 @@
+PI = 3.1415926535
+
+print("Value of PI:", PI)
+print("Type of PI:", type(PI))
+
+small_num = 2.5e-3 # 2.5 * 10^(-3) or 0.0025
+
+print(f"Value of small_num= {small_num}")
+print(f"Type of small_num:  {type(small_num)}")
