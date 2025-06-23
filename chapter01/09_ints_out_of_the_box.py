43b80c7b71ed2cc5927a4332b7ec5d1ac316a5cc
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-06-23 19:24:49 +0300

    print with _add_ method

---
+++ chapter01/09_ints_out_of_the_box.py
@@ -0,0 +1,13 @@
+a = 10
+b = 20
+
+result = a + b
+
+print(f"{a} + {b} = {result}")
+print("------------------")
+
+print("Type of a:", type(a))
+print(f"Type of a: {type(a)}")
+
+magic_result = a.__add__(b)
+print(f"Magic result: {magic_result}")
