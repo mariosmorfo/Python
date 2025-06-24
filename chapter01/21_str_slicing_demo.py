b5ff7d234367fd01a7266ad19adc4b96d48d51c4
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-06-24 20:10:13 +0300

    slicing demo

---
+++ chapter01/21_str_slicing_demo.py
@@ -0,0 +1,28 @@
+s = "Coding Factory"
+
+s1 = s[7] #F
+print(s1)
+
+s2 = s[::]
+print(s2)
+
+s3 = s[7:]
+print(s3)
+
+s4 = s[-2]
+print(s4)
+
+s5 = s[:-1]
+print(s5)
+
+#Factor
+s6 = s[7:-1]
+print(s6)
+
+s7 = s[7:7]
+print(s7)
+
+s = "Coding Factory"
+s_rev = s[::-1]
+print(s_rev)
+
