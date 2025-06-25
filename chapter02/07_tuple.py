957eb7dd2dcfbdf5af10e0fc23f8d1e10d5b8caf
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-06-25 14:02:22 +0300

    convert tuple to list for change a name and convert again to tuple

--- chapter02/07_tuple.py
+++ chapter02/07_tuple.py
@@ -13,3 +13,9 @@ first_st, second_st, third_st = students  #students[0], students[1], students[2]
 print(f"first_st: {first_st}")
 print(f"second_st: {second_st}")
 print(f"third_st: {third_st}")
+
+students = list(students)
+students[0] = "Helen"
+students = tuple(students)
+
+print(f"{students}, {type(students)}")
