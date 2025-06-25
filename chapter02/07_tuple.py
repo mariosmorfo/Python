7f2e205c8ac022986ce4516b9e48797eb81a7e55
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-06-25 13:56:54 +0300

    defile tuple with iterations

---
+++ chapter02/07_tuple.py
@@ -0,0 +1,15 @@
+# define tuple
+students = "Alice", "Bob", "Charlie"
+print(f"{students} : {type(students)}")
+
+#iteration
+for index, student in enumerate(students):
+    print(f"{index} {student}")
+
+for student in students:
+    print(student)
+
+first_st, second_st, third_st = students  #students[0], students[1], students[2]
+print(f"first_st: {first_st}")
+print(f"second_st: {second_st}")
+print(f"third_st: {third_st}")
