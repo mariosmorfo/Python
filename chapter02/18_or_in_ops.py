92ecb179e92978d288314d042a86ea2584175db4
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-06-27 20:15:19 +0300

    quiz game

---
+++ chapter02/18_or_in_ops.py
@@ -0,0 +1,15 @@
+choice = "q"
+
+if choice == 'q' or choice == "Q":
+  print("Quiz")
+else:
+  print("Continue")
+
+if choice.upper() == "Q":
+  print("Quiz")
+else:
+  print("Continue")
+
+
+  if choice in ('q', 'Q'):
+    print("Quit")
