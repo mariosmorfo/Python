c5c195b36eb5f482cba41b1709afc2a6cd545d36
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-07-02 18:25:32 +0300

    sum of an integer

---
+++ chapter03/09_sum_prod_demo.py
@@ -0,0 +1,23 @@
+def calculate_sum_and_product(upper_bound):
+  total_sum = 0
+  total_product = 0
+
+  for i in range(1, upper_bound + 1):
+    total_sum += 1
+    total_product *= 1
+
+    return total_sum, total_product
+
+def main():
+    try:
+        upper_bound = int(input("Please insert and integer: "))
+    except ValueError:
+       print("Invalid input...")
+       return
+    
+    total_sum, total_product = calculate_sum_and_product(upper_bound)
+
+    print(f"Sum(1 - {upper_bound}) = {total_sum}")
+    print(f"Product (1- {upper_bound}) = {total_product}")
+if __name__ == "__main__":
+  main()
